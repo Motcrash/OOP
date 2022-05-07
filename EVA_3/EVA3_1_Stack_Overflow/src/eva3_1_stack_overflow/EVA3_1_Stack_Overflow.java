4e07e6551f344e7884806afc700df87e36502fb6
Motcrash <89164397+Motcrash@users.noreply.github.com>
2022-05-06 23:59:13 -0600

    Practicas EVA 3

---
+++ EVA_3/EVA3_1_Stack_Overflow/src/eva3_1_stack_overflow/EVA3_1_Stack_Overflow.java
@@ -0,0 +1,16 @@
+package eva3_1_stack_overflow;
+
+public class EVA3_1_Stack_Overflow {
+
+    public static void main(String[] args) {
+        int c = 0;
+        terminarStack(c);
+    }
+    
+    public static void terminarStack(int c){
+        c++;
+        System.out.println(c);
+        terminarStack(c);   
+    }
+    
+}
